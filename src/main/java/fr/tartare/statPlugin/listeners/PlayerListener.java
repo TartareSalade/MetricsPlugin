e0e8bd231a1b80e7ee30895ab7930c2f05d20dfd
sany <sany.bakir@epita.fr>
2026-06-14 20:01:04 +0200

    feat: add block break listener

--- src/main/java/fr/tartare/statPlugin/listeners/PlayerListener.java
+++ src/main/java/fr/tartare/statPlugin/listeners/PlayerListener.java
@@ -1,5 +1,6 @@
 package fr.tartare.statPlugin.listeners;
 
+import net.kyori.adventure.text.ComponentLike;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -52,5 +53,26 @@ public class PlayerListener implements Listener {
     public void onBreakEvent(BlockBreakEvent event){
         Class<? extends Event> eventType = event.getClass();
         String eventName = eventType.getSimpleName();
+        /*
+        get eventType
+        playerUUID
+        playerName
+        blockType
+        worldName
+        timestamp
+        pos(x,y,z)
+         */
+        Player player = event.getPlayer();
+        UUID playerUUID = player.getUniqueId();
+        String playerName = String.valueOf(player.displayName());
+        String blockType = event.getBlock().getType().toString();
+        String worldName = player.getWorld().getName();
+        //var pos = player.getPose();
+        long timestamp = System.currentTimeMillis();
+        var pos = player.getLocation();
+        player.sendMessage("DEBUG POS + ", String.valueOf(pos));
+        player.sendMessage("DEBUG PLAYER NAME + ", playerName);
+        System.out.println("[MetricPlugin] " + eventName + " triggered by player " + playerName + " (UUID: " + playerUUID + ") in world " + worldName + " at timestamp " + timestamp + " breaking block of type " + blockType);
     }
+
 }
